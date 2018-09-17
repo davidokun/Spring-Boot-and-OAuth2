631ee09133f4b8899e40ac335207b31d1fde44f7
davidokun <davidokun99@gmail.com>
2018-09-17 10:47:38 +0100

    Adding security configuration.

--- src/main/java/com/singleton/apps/springbootoauth2/SpringBootOauth2Application.java
+++ src/main/java/com/singleton/apps/springbootoauth2/SpringBootOauth2Application.java
@@ -3,12 +3,38 @@ package com.singleton.apps.springbootoauth2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @SpringBootApplication
 @EnableOAuth2Sso
-public class SpringBootOauth2Application {
+@RestController
+public class SpringBootOauth2Application extends WebSecurityConfigurerAdapter {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootOauth2Application.class, args);
     }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .antMatcher("/**")
+            .authorizeRequests()
+            .antMatchers("/", "/login**", "/webjars/**", "/error**")
+            .permitAll()
+            .anyRequest()
+            .authenticated();
+    }
+
+    @RequestMapping("/user")
+    public Principal user(Principal principal) {
+        return principal;
+    }
+
+
+
 }
